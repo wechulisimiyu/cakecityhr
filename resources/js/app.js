aa170b67df157d2c5551266b0c33e046f18f3209
Wechuli Simiyu <wechuli017@gmail.com>
2024-12-06 14:37:27 +0300

    add full calendar

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,23 @@
-console.log('Hello World')
+// import '@fullcalendar/core/main.css'
+// import '@fullcalendar/daygrid/main.css'
+import { Calendar } from '@fullcalendar/core'
+import dayGridPlugin from '@fullcalendar/daygrid'
+
+window.initCalendar = function(events) {
+    const calendarEl = document.getElementById('calendar')
+    if (!calendarEl) return
+  
+    const calendar = new Calendar(calendarEl, {
+      plugins: [dayGridPlugin],
+      initialView: 'dayGridMonth',
+      events: events,
+      headerToolbar: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,dayGridWeek'
+      },
+      height: 'auto'
+    })
+  
+    calendar.render()
+  }
