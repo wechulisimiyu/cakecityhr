a44e7fbc0077285d589a4b3b9c236ad289e37a96
Wechuli Simiyu <wechuli017@gmail.com>
2025-01-05 15:09:57 +0300

    fix SSL errors

--- config/database.ts
+++ config/database.ts
@@ -12,9 +12,7 @@ const dbConfig = defineConfig({
         user: env.get('DB_USER'),
         password: env.get('DB_PASSWORD'),
         database: env.get('DB_DATABASE'),
-        ssl: {
-          rejectUnauthorized: false,
-        },
+        ssl: false,
       },
       migrations: {
         naturalSort: true,
