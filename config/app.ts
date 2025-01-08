548f8eafc66baa63574b6f9463cdf5a1fdd2b413
Wechuli Simiyu <wechuli017@gmail.com>
2025-01-09 01:45:32 +0300

    cookie set to secure

--- config/app.ts
+++ config/app.ts
@@ -1,5 +1,5 @@
 import env from '#start/env'
-import app from '@adonisjs/core/services/app'
+// import app from '@adonisjs/core/services/app'
 import { Secret } from '@adonisjs/core/helpers'
 import { defineConfig } from '@adonisjs/core/http'
 
@@ -35,7 +35,7 @@ export const http = defineConfig({
     path: '/',
     maxAge: '2h',
     httpOnly: true,
-    secure: app.inProduction,
+    secure: true,
     sameSite: 'lax',
   },
 })
