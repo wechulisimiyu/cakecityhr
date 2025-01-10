adcdbdded4db124b0a1bdf0abd039c62c8e13d44
Wechuli Simiyu <wechuli017@gmail.com>
2025-01-10 22:52:01 +0300

    fix csrf field

--- config/shield.ts
+++ config/shield.ts
@@ -16,7 +16,7 @@ const shieldConfig = defineConfig({
    * to learn more
    */
   csrf: {
-    enabled: false,
+    enabled: true,
     exceptRoutes: [],
     enableXsrfCookie: false,
     methods: ['POST', 'PUT', 'PATCH', 'DELETE'],
